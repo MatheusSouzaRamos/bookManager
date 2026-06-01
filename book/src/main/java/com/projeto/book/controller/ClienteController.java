712e35730db198bb258726bf463dc28554b10e0e
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-06-01 18:46:59 -0300

    base dos controllers

---
+++ book/src/main/java/com/projeto/book/controller/ClienteController.java
@@ -0,0 +1,57 @@
+package com.projeto.book.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.book.dto.ClienteDTO;
+import com.projeto.book.service.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        List<ClienteDTO> dto = service.findAll();
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> findById(@PathVariable Long id){
+        ClienteDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClienteDTO> insert(@RequestBody ClienteDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClienteDTO> update(@PathVariable Long id, @RequestBody ClienteDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClienteDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ book/src/main/java/com/projeto/book/controller/LivroController.java
@@ -0,0 +1,58 @@
+package com.projeto.book.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.book.dto.LivroDTO;
+import com.projeto.book.service.LivroService;
+
+@RestController
+@RequestMapping(value = "/livros")
+public class LivroController {
+    @Autowired
+    private LivroService service;
+
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> findAll(){
+        List<LivroDTO> dto = service.findAll();
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<LivroDTO> findById(@PathVariable Long id){
+        LivroDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<LivroDTO> insert(@RequestBody LivroDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LivroDTO> update(@PathVariable Long id, @RequestBody LivroDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<LivroDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
