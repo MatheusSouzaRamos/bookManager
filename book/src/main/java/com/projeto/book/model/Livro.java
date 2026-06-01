ff1fe0b63baa40c05e54c9f819fe61f37671a189
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-06-01 20:02:11 -0300

    iniciando front+js

--- book/src/main/java/com/projeto/book/model/Livro.java
+++ book/src/main/java/com/projeto/book/model/Livro.java
@@ -3,7 +3,6 @@ package com.projeto.book.model;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
