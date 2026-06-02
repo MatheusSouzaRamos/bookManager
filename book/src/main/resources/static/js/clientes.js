455c69baae5461d7e85cebaff73d7a58a9a532a6
MatheusSouzaRamos <144972414+MatheusSouzaRamos@users.noreply.github.com>
2026-06-02 15:36:04 -0300

    cards crud e tabelas

--- book/src/main/resources/static/js/clientes.js
+++ book/src/main/resources/static/js/clientes.js
@@ -11,7 +11,35 @@ function listarTodosClientes(){
         return res.json();
     })
     .then(data => {
-        console.log("Clientes: ", data);
+        // console.log("Clientes: ", data);
+        let dados = document.getElementById("tabelaClientes");
+        let linhas = ""
+
+        for(let dado of data){
+            linhas += `
+                <tr>
+                    <td>${dado.id}</td>
+                    <td>${dado.nome}</td>
+                    <td>${dado.telefone}</td>
+                    <td>${dado.endereco}</td>
+                    <td>${dado.cpf}</td>
+                </tr>
+            
+            `
+        }
+
+        dados.innerHTML = `
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Telefone</th>
+                    <th>Endereço</th>
+                    <th>CPF</th>
+                </tr>
+                ${linhas}
+            </table>
+        `
     })
     .catch(erro => {
         console.log("Erro: ", erro);
@@ -39,9 +67,61 @@ function buscarClienteId(){
     })
     .then(data => {
         console.log("Cliente: ", data);
+        let dados = document.getElementById("tabelaClientes");
+
+        dados.innerHTML = `
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Telefone</th>
+                    <th>Endereço</th>
+                    <th>CPF</th>
+                </tr>
+                <tr>
+                    <td>${data.id}</td>
+                    <td>${data.nome}</td>
+                    <td>${data.telefone}</td>
+                    <td>${data.endereco}</td>
+                    <td>${data.cpf}</td>
+                </tr>
+            </table>
+        `
     })
-    .then(erro => {
+    .catch(erro => {
         console.log("Erro: ", erro);
+    })
+}
+
+function inserirCliente(){
+    let nomeInserir = document.getElementById("nomeInserirCliente").value;
+    let enderecoInserir = document.getElementById("enderecoInserirCliente").value;
+    let cpfInserir = document.getElementById("cpfInserirCliente").value;
+    let telefoneInserir = document.getElementById("telefoneInserirCliente").value;
+
+    if(!nomeInserir || nomeInserir.trim() === "" || !enderecoInserir || enderecoInserir.trim() === "" || !cpfInserir || cpfInserir.trim() === "" || !telefoneInserir || telefoneInserir.trim() === ""){
+        alert("Campos inválidos, confira os campos de edição.");
+        return;
+    }
+
+    fetch(`http://localhost:8080/clientes`, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            cpf: cpfEditar,
+            endereco: enderecoEditar,
+            nome: nomeEditar,
+            teledone: telefoneEditar,
+        })
+    })
+    .then(res => {
+        if(!res.ok) throw new Error("Erro ao inserir cliente.")
+    })
+    .catch(erro => {
+        console.log("Erro: ", erro)
     })
 }
 
